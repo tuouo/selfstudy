83ca2c564bbc675d916b35d64a1e44142cd0e8c3
tuouo <ltytuotuo@gmail.com>
2015-09-04 23:50:41 +0800

    cilent ip

--- 0062TCPclient.py
+++ 0062TCPclient.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 9999))
+
+clientIP = '127.0.0.1'
+s.connect((clientIP, 9998))
 print(s.recv(1024).decode('utf-8'))
 for data in [b'haha', b'll', b'mess']:
     s.send(data)
