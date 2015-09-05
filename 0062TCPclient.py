64f6f40497498fe66892b7659080b3e89ed9821d
tuouo <ltytuotuo@gmail.com>
2015-09-05 22:49:38 +0800

    remove self...

--- 0062TCPclient.py
+++ 0062TCPclient.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+clientIP = ''
 clientIP = '127.0.0.1'
 s.connect((clientIP, 9998))
 print(s.recv(1024).decode('utf-8'))
