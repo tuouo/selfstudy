feb892620798ea5b15069aaf2123978a26322193
tuouo <ltytuotuo@gmail.com>
2015-09-08 16:18:15 +0800

    start service, %, commit

---
+++ 0071mysql.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import mysql.connector
+
+# mysql service must start before this program run.
+# or can't connect
+
+conn = mysql.connector.connect(user = 'root', password = '', database = 'test')
+cursor = conn.cursor()
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into user (id, name) values(%s, %s)', ['1', 'tuouo'])		# %
+print("rowcount = ", cursor.rowcount)
+conn.commit()					# don't forget
+cursor.close()
+
+cursor = conn.cursor()
+cursor.execute('select * from user where id = %s', ['1'])
+values = cursor.fetchall()
+print(values)
+cursor.close()
+conn.close()
