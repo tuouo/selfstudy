f2568fe6f1c4864f596c4ed018a5a744599e6949
tuouo <ltytuotuo@gmail.com>
2015-09-12 00:32:18 +0800

    ModelMetaclass

--- www/orm.py
+++ www/orm.py
@@ -49,6 +49,7 @@ def execute(sql, args, autocommit = True):
             raise 
         return affected
 
+
 class Field(object):
     def __init__(self, name, colum_type, primary_key, default):
         self.name = name
@@ -78,6 +79,46 @@ class FloatField(Field):
 class TextField(Field):
     def __init__(self, name = None, default = None):
         super().__init__(name, 'text', False, default)
+
+
+class ModelMetaclass(type):
+    def __new__(class, name, bases, attrs):
+        if __name__ = 'Model':									# ignore Model self
+            return type.__new(class, name, bases, attrs)	
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table %s)' % (name, tableName))	
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs:
+            if isinstance(v, Field):
+                logging.info("\tFound mapping: %s ==> %s" % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    if primaryKey:
+                        raise RuntimeError("Duplicate primary key for field: %s" % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError("Primary key not found")
+        for k in mappings.keys():
+            attrs.pop(k)
+        esacFields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mapping__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primaryKey__'] = primaryKey
+        attrs['__fields__'] = fields
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primary, ', '.join(esacFields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, 
+        	', '.join(esacFields), primary, creArgsStr(len(esacFields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s` = ?' % (tableName, 
+        	', '.join(map(lambda f: '`%s` = ? ' % mappings.get(f).name or f, fields)), primary)
+        attrs['__delete__'] = 'delete from `%s` where `%s` = ?' % (tableName, primary)
+        return type.__new(class, name, bases, attrs)
+        
+
+
 
 
 class Model(dict, metaclass = ModleMetaclass):
