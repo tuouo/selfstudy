f960ff027328479eef24dc69702daa355b88c612
tuouo <ltytuotuo@gmail.com>
2015-09-19 22:01:38 +0800

    source

--- www/schema.sql
+++ www/schema.sql
@@ -1,45 +1,45 @@
--- schema.sql
+net-- schema.sql
 
 drop database if exists awesome;
-create table awesome;
+create database awesome;
 use awesome;
 
 grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
 
 create table users(
-    `id` varchar(50) not null,
+    `user_id` varchar(50) not null,
     `email` varchar(50) not null,
     `password` varchar(50) not null,
     `admin` bool not null,
     `name` varchar(50) not null,
     `image` varchar(500) not null,
-    `create_at` real not null,
-    unique key `idx_email`(`email`),
-    key `idx_create_at`(`create_at`),
-    primary key(`id`)
-)engine = innoodb default charset = utf8;
+    `created_at` real not null,
+    unique key `idx_email` (`email`),
+    key `idx_created_at` (`created_at`),
+    primary key (`user_id`)
+) engine = innodb default charset = utf8;
 
 create table blogs(
-    `id` varchar(50) not null,
+    `blog_id` varchar(50) not null,
     `user_id` varchar(50) not null,
     `user_name` varchar(50) not null,
     `user_image` varchar(500) not null,
     `name` varchar(50) not null,
     `summary` varchar(200) not null,
     `content` mediumtext not null,
-    `create_at` real not null,
-    key `idx_create_at`(`create_at`),
-    primary key(`id`)
-)engine = innoodb default charset = utf8;
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`blog_id`)
+) engine = innodb default charset = utf8;
 
 create table comment(
-    `id` varchar(50) not null,
+    `comment_id` varchar(50) not null,
     `blog_id` varchar(50) not null,
     `user_id` varchar(50) not null,
     `user_name` varchar(50) not null,
     `user_image` varchar(500) not null,
     `content` mediumtext not null,
-    `create_at` real not null,
-    key `id_create_at`(`create_at`)
-    primary key(`id`)
-)engine = innoodb default charset = utf8;
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`comment_id`)
+) engine = innodb default charset = utf8;
