d61acdfd630ea47a6e53c7fbb75d55fef2d8522b
tuouo <ltytuotuo@gmail.com>
2015-09-19 21:57:27 +0800

    table name

--- www/models.py
+++ www/models.py
@@ -7,32 +7,32 @@ def next_id():
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
-    __tables__ = 'users'
-    id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
+    __table__ = 'users'
+    user_id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
     email = StringField(ddl = 'varchar(50)')
     password = StringField(ddl = 'varchar(50)')
     admin = BooleanField()
     name = StringField(ddl = 'varchar(50)')
     image = StringField(ddl = 'varchar(500)')
-    create_at = FloatField(default = time.time)
+    created_at = FloatField(default = time.time)
 
 class Blog(Model):
-    __tables__ = 'blogs'
-    id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
+    __table__ = 'blogs'
+    blog_id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
     user_id = StringField(ddl = 'varchar(50)')
     user_name = StringField(ddl = 'varchar(50)')
     user_image = StringField(ddl = 'varchar(500)')
     name = StringField(ddl = 'varchar(50)')
     summary = StringField(ddl = 'varchar(200)')
     content = TextField()
-    create_at = FloatField(default = time.time)
+    created_at = FloatField(default = time.time)
 
 class Comment(Model):
-    __tables__ = 'comments'
-    id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
+    __table__ = 'comment'
+    comment_id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
     blog_id = StringField(ddl = 'varchar(50)')
     user_id = StringField(ddl = 'varchar(50)')
     user_name = StringField(ddl = 'varchar(50)')
     user_image = StringField(ddl = 'varchar(500)')
     context = TextField()
-    create_at = FloatField(default = time.time)
+    created_at = FloatField(default = time.time)
