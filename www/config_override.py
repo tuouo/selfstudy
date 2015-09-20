ff4cc6d8fb10e1f3ad41e627abfcf7dda9bb9f95
tuouo <ltytuotuo@gmail.com>
2015-09-20 22:55:11 +0800

    things to override default

---
+++ www/config_override.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''Default Override. (Not change config_default directed for protect)'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
