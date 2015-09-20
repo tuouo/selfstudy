2322d4c61d686c272b99bce847de5716203a2a7d
tuouo <ltytuotuo@gmail.com>
2015-09-20 22:56:10 +0800

    dict support x.y

---
+++ www/DictSupportPoint.py
@@ -0,0 +1,24 @@
+#!/usr/bin/ env python3
+# -*- coidng: utf-8 -*-
+'''Simple dict with support access as x.y style'''
+
+class Dict():
+    def __init__(self, names(), values(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError("'Dict' object has not attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+def toDict(d):
+    D = Dict():
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
