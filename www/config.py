3df5ca96e05377cbbda4047dbd41f1fc7d9a93ac
tuouo <ltytuotuo@gmail.com>
2015-09-20 22:52:07 +0800

    support x.y

---
+++ www/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def merge(default, override):
+    r = {}
+    for k, v in default.items():
+        r[k] = (merge(v, override[k]) if isinstance(v, dict) else override[k]) if k in override else v
+    return r
+
+import config_default
+configs = config_default.configs
+try:
+    import config_override
+    configs = merge(configs, config_override.configs)
+except ImportError:
+    pass	# if no config_override.py
+
+import DictSupportPoint as Dict
+configs = Dict.toDict(configs)
