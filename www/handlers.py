d83455668a2fa8a1cc2c1c27cc00ce751f3a8f2f
tuouo <ltytuotuo@gmail.com>
2015-09-22 22:26:24 +0800

    '/'

---
+++ www/handlers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/envpython3
+# -*- coding: utf-8 -*-
+' url handlers'
+
+import re, time, json, logging, hashlib, base64, asyncio
+from coroweb import get, post
+from models import User, Comment, Blog, next_id
+
+@get('/')
+def index(request):
+    logging.info('url handlers	index')
+    users = yield from User.findAll()
+    logging.info('in index:User ok.')
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
