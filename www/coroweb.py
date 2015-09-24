e0dc4134158c6d111bf7b5d46a80a4205a0f37d0
tuouo <ltytuotuo@gmail.com>
2015-09-24 23:46:15 +0800

    handles added

--- www/coroweb.py
+++ www/coroweb.py
@@ -3,7 +3,7 @@
 import asyncio, functools, inspect, logging, os
 from urllib import parse
 from aiohttp import web
-#from apis import AIPError
+from apis import AIPError
 
 def get(path):
     '''Define decorator @ get('/path')'''
@@ -27,16 +27,113 @@ def post(path):
         return wrapper
     return decorator
 
+def has_request_arg(fn):
+    sig = inspect.singnature(fn)
+    params = sig.parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found:
+            if (param.kind != inspect.Parameter.VAR_POSITIONAL and
+                param.kind != inspect.Parameter.KEYWORD_ONLY and
+                param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+    return found
+
+def hasarg(fn, things):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == things:
+            return True
+
+def has_var_kw_arg(fn):
+    hasarg(fn, inspect.Parameter.VAR_KEYWORD)
+
+def has_named_kw_args(fn):
+    hasarg(fn, inspect.Parameter.KEYWORD_ONLY)
+
+def get_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+           args.append(name)
+    return tuple(args)
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and
+           param.default == inspect.Parameter.empty:
+           args.append(name)
+    return tuple(args)
+
+
 class RequestHandlee(object):
     def __init__(self, app, fn):
         self._app = app
         self._fn = fn
+        seld._has_request_arg = has_request_arg(fn)
+        seld._has_var_kw_arg = has_var_kw_arg(fn)
+        seld._has_named_kw_args = has_named_kw_args(fn)
+        seld._named_kw_args = get_named_kw_args(fn)
+        seld._required_kw_args = get_required_kw_args(fn)
 
     @asyncio.coroutine
     def __call__(self, request):
         kw = None
-        r = yield from self.func(**kw)
-        return r
+        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
+            if request.method = 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing content-type')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    parmas = yield from request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = yeild from request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs.True).items():
+                        kw[k] = v[0]
+        if kw == None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                # remove all unnamed kw
+                copy = dict()
+                for name in _named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            for k, v in request.match_info.items():
+                # check named arg:
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        # check request kw:
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
+        logging.info("call with args: %s" % str(kw))
+        try:
+            r = yield from self.func(**kw)
+            return r
+        except AIPError as e:
+            return dict(error = e.error, data = e.data, message = e.message)
 
 def add_route(app, fn):
     method = getattr(fn, '__method__', None)
