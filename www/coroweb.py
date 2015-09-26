dfac87f899c80d41ab28d85415a7b6a36f4712fe
tuouo <ltytuotuo@gmail.com>
2015-09-26 09:47:48 +0800

    __route__ get post __call__

--- www/coroweb.py
+++ www/coroweb.py
@@ -3,7 +3,7 @@
 import asyncio, functools, inspect, logging, os
 from urllib import parse
 from aiohttp import web
-from apis import AIPError
+from apis import APIError
 
 def get(path):
     '''Define decorator @ get('/path')'''
@@ -13,6 +13,7 @@ def get(path):
             return func(*args, **kw)
         wrapper.__method__ = 'GET'
         wrapper.__route__ = path
+        logging.info('in get, coroweb')
         return wrapper
     return decorator
 
@@ -24,11 +25,12 @@ def post(path):
             return func(*args, **kw)
         wrapper.__mathod__ = 'POST'
         wrapper.__route__ = path
+        logging.info('in post, coroweb')
         return wrapper
     return decorator
 
 def has_request_arg(fn):
-    sig = inspect.singnature(fn)
+    sig = inspect.signature(fn)
     params = sig.parameters
     found = False
     for name, param in params.items():
@@ -39,7 +41,7 @@ def has_request_arg(fn):
             if (param.kind != inspect.Parameter.VAR_POSITIONAL and
                 param.kind != inspect.Parameter.KEYWORD_ONLY and
                 param.kind != inspect.Parameter.VAR_KEYWORD):
-            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+                raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
     return found
 
 def hasarg(fn, things):
@@ -66,27 +68,27 @@ def get_required_kw_args(fn):
     args = []
     params = inspect.signature(fn).parameters
     for name, param in params.items():
-        if param.kind == inspect.Parameter.KEYWORD_ONLY and
-           param.default == inspect.Parameter.empty:
+        if (param.kind == inspect.Parameter.KEYWORD_ONLY and
+            param.default == inspect.Parameter.empty):
            args.append(name)
     return tuple(args)
 
 
-class RequestHandlee(object):
+class RequestHandler(object):
     def __init__(self, app, fn):
         self._app = app
         self._fn = fn
-        seld._has_request_arg = has_request_arg(fn)
-        seld._has_var_kw_arg = has_var_kw_arg(fn)
-        seld._has_named_kw_args = has_named_kw_args(fn)
-        seld._named_kw_args = get_named_kw_args(fn)
-        seld._required_kw_args = get_required_kw_args(fn)
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_named_kw_args = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
 
     @asyncio.coroutine
     def __call__(self, request):
         kw = None
         if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
-            if request.method = 'POST':
+            if request.method == 'POST':
                 if not request.content_type:
                     return web.HTTPBadRequest('Missing content-type')
                 ct = request.content_type.lower()
@@ -96,7 +98,7 @@ class RequestHandlee(object):
                         return web.HTTPBadRequest('JSON body must be object.')
                     kw = params
                 elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
-                    params = yeild from request.post()
+                    params = yield from request.post()
                     kw = dict(**params)
                 else:
                     return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
@@ -104,7 +106,7 @@ class RequestHandlee(object):
                 qs = request.query_string
                 if qs:
                     kw = dict()
-                    for k, v in parse.parse_qs(qs.True).items():
+                    for k, v in parse.parse_qs(qs, True).items():
                         kw[k] = v[0]
         if kw == None:
             kw = dict(**request.match_info)
@@ -130,25 +132,25 @@ class RequestHandlee(object):
                     return web.HTTPBadRequest('Missing argument: %s' % name)
         logging.info("call with args: %s" % str(kw))
         try:
-            r = yield from self.func(**kw)
+            r = yield from self._fn(**kw)
             return r
-        except AIPError as e:
+        except APIError as e:
             return dict(error = e.error, data = e.data, message = e.message)
 
 def add_route(app, fn):
     method = getattr(fn, '__method__', None)
-    path = getattr(fn, '__path__', None)
+    path = getattr(fn, '__route__', None)
+    logging.info('method:%s\tpath:%s' %(method, path))
     if method is None or path is None:
         raise ValueError('@get or @post not defined in %s.' % str(fn))
     if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
         fn = asyncio.coroutine(fn)
-    logging.info('add rout %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.singnature(fn).parameters.keys())))
-    app.router.add_route(mathod, path, RequestHandler(app, fn))
+    logging.info('add rout %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
 
 def add_routes(app, module_name):
-    logging.info('in add_routes')
     n = module_name.rfind('.')
-    logging.info('n: %s' % n)
+    logging.info('in add_routes(n: %s)' % n)
     if n == (-1):
         mod = __import__(module_name, globals(), locals())
     else:
@@ -161,7 +163,7 @@ def add_routes(app, module_name):
         fn = getattr(mod, attr)
         if callable(fn):
             method = getattr(fn, '__method__', None)
-            path = getattr(fn, '__path__', None)
+            path = getattr(fn, '__route__', None)
             if method and path:
                 add_route(app,fn)
 
