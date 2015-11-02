a68413ff651b91e41d629151d010f9f08e3796f8
tuouo <ltytuotuo@gmail.com>
2015-11-02 17:52:23 +0800

    () for speed up

--- diveIntoPython3/approximateSize.py
+++ diveIntoPython3/approximateSize.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-suffixes = {1000 : ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-            1024 : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+suffixes = {1000 : ('KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'),
+            1024 : ('KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB')}
 
 def approximate_size(size, kilobyte_is_1024_bytes = True):
     '''Convert a file size to short.
