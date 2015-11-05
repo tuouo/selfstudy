071350b44b5e21dc3b598f5682a4575f88bcf484
tuouo <ltytuotuo@gmail.com>
2015-11-05 22:31:48 +0800

    awesome image gravater"
    ;

--- www/handlers.py
+++ www/handlers.py
@@ -5,6 +5,7 @@
 import re, time, json, logging, hashlib, base64, asyncio
 from coroweb import get, post
 from models import User, Comment, Blog, next_id
+from apis import  APIValueError, APIResourceNotFoundError
 
 @get('/test')
 def index(request):
@@ -49,3 +50,31 @@ def api_get_user(*, page = '1'):
         u.password = '******'
     #return dict(page = p, users = users)
     return dict(user = users)
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-_]+\@[a-z0-9\-_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+@post('/api/users')
+def api_register_user(*, email, name, password):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not password or not _RE_SHA1.match(_RE_SHA1):
+        raise APIValueError('_RE_SHA1')
+    users = yield from User.findAll('email=?',  [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    sha1_password = '%S:%S' % (uid, password)
+    user = User(id = uid, name = name.strip(), email = email,
+     password = hashlib.sha1(sha1_password.encode('utf-8').hexdigest()),
+     image = 'http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from user.save()
+    # make session cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age = 86400, httponly = True)
+    user.password = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii = False).encode('utf-8')
+    return r
