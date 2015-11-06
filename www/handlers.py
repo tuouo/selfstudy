d2fff3a5ad1a1a314eef91a91e26433d7c516305
tuouo <ltytuotuo@gmail.com>
2015-11-06 19:31:28 +0800

    register post:user,auth

--- www/handlers.py
+++ www/handlers.py
@@ -6,6 +6,13 @@ import re, time, json, logging, hashlib, base64, asyncio
 from coroweb import get, post
 from models import User, Comment, Blog, next_id
 from apis import  APIValueError, APIResourceNotFoundError
+from config import configs
+
+#import markdown2
+from aiohttp import web
+
+COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
 
 @get('/test')
 def index(request):
@@ -51,6 +58,26 @@ def api_get_user(*, page = '1'):
     #return dict(page = p, users = users)
     return dict(user = users)
 
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+@get('/signout')
+def signout():
+    referer = request.header.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age = 0, httponly = True)
+    logging.info('user signed out.')
+    return r
+
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-_]+\@[a-z0-9\-_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
@@ -78,3 +105,65 @@ def api_register_user(*, email, name, password):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii = False).encode('utf-8')
     return r
+
+@post('/api/authenticate')
+def authenticate(*, email, password):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not password:
+        raise APIValueError('password', 'Invalid password.')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not existt.')
+    user = user[0]
+
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(password.encode('utf-8'))
+    if user.password != sha1.hexdigest():
+        raise APIValueError('password', 'Invalid password.')
+    # Authenticate is ok, then set cookie
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age = 86400, httponly = True)
+    user.password = '******'
+    r.content_type = 'application/json'
+    r.body = json.dump(user, ensure_ascii = False).encode('utf-8')
+    return r
+
+
+def user2cookie(user, max_age):
+     '''
+     Generate cookie str by user.
+     '''
+     expires = str(int(time.time() + max_age))
+     s = '%s-%s-%s-%s' % (uid, user.password, expires, _COOKIE_KEY)
+     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest]
+     return '-'.join(L)
+
+@asyncio.coroutine
+def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user when cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = yield from User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.password, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1 when decode')
+            return None
+        user.password = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
