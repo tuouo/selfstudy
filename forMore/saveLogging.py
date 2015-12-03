a2c385749a5315e1cc535c429a20f847c79a95de
tuouo <ltytuotuo@gmail.com>
2015-12-03 17:18:14 +0800

    save logging test

---
+++ forMore/saveLogging.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+logging.basicConfig(
+    level    = logging.INFO,
+    format   = '|%(asctime)s|%(filename)s[line:%(lineno)s] %(levelname)s\n%(message)s',
+    datefmt  = '%a, %d %b %Y %H:%M:%S',
+    filename = 'test.log',
+    filemode = 'w'
+	)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+logging.info("This is logging test of INFO")
+logging.debug("This is logging test of debug")
+logging.warning("This is logging test of warning")
