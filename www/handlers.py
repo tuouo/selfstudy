8795d5aaa0af013efe4f0ccb9dfea4c4541203b2
tuouo <ltytuotuo@gmail.com>
2015-12-11 23:48:27 +0800

    blongs create

--- www/handlers.py
+++ www/handlers.py
@@ -194,3 +194,17 @@ def authenticate(*, email, password):
     return r
 
 
+@post('/api/blogs')
+def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty.')
+    blog = Blog(user_id = request.__user__.user_id, user_name = request.__user__.name, user_image = request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip())
+    yield from blog.save()
+    return blog
+
+
