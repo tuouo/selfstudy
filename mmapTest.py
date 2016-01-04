c95802ef2be1bc661e5ca62b004e447a4dc4bd10
tuouo <ltytuotuo@gmail.com>
2016-01-04 23:44:48 +0800

    need more

--- mmapTest.py
+++ mmapTest.py
@@ -2,6 +2,21 @@
 # -*- coding: utf-8 -*-
 import os, mmap
 path = os.path.join(os.getcwd(), "resource", "zangnan", "test.rtf")
+with open(path, "r+b") as f:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################
 # with open(path, "wb") as f:
 #     f.write(b"Hello Python!\nPython Hello!\n")
 
@@ -15,14 +30,14 @@ path = os.path.join(os.getcwd(), "resource", "zangnan", "test.rtf")
 #     mm.close()
 
 
-mm = mmap.mmap(-1, 14)
-mm.write(b"Hello world!")
-pid = os.getpid()
-print(pid)
-# if pid == 0:
-#     mm.seek(0)
-#     print(mm.readline())
-#     mm.close()
-mm.seek(0)
-print(mm.readline())
-mm.close()
+# mm = mmap.mmap(-1, 14)
+# mm.write(b"Hello world!")
+# pid = os.getpid()
+# print(pid)
+# # if pid == 0:
+# #     mm.seek(0)
+# #     print(mm.readline())
+# #     mm.close()
+# mm.seek(0)
+# print(mm.readline())
+# mm.close()
