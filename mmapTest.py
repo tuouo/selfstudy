7d9f243fc6348fdb8e84d5b459dd6596fc4dcbc5
tuouo <ltytuotuo@gmail.com>
2016-01-05 17:30:17 +0800

    move in byte view

--- mmapTest.py
+++ mmapTest.py
@@ -2,9 +2,34 @@
 # -*- coding: utf-8 -*-
 import os, mmap
 path = os.path.join(os.getcwd(), "resource", "zangnan", "test.rtf")
-with open(path, "r+b") as f:
-    
+with open (path, 'w') as f:
+    # print("fileno:", f.fileno())
+    for i in range(5):
+        f.write("Hello, this is line %s.\n" % (i * str(i)))
 
+with open(path, "r+b") as f:
+    mm = mmap.mmap(f.fileno(), 0)
+    # print("fileno:", f.fileno())
+    # print(mm.readline())
+    # print(mm[:5])
+    # print(mm[6:])
+    # mm.seek(0)
+    # print(mm.readline())
+    # mm.close()
+    location = mm.find(b"2")
+    print(location)
+    print(mm.readline())
+    lineThis = mm.rfind(b'\n', 0, location)
+    print(lineThis)
+    mm.seek(lineThis + 1)
+    mm.readline()
+    linelen = len(mm.readline())
+    print(mm.tell())
+    print(mm[lineThis + 1: mm.tell()])
+    mm.move(lineThis + 1, mm.tell() - linelen, linelen)
+
+
+    
 
 
 
