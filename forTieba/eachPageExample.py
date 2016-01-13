ace79791f83ef0678fd6563669ea6da786fcf375
tuouo <ltytuotuo@gmail.com>
2016-01-13 15:40:39 +0800

    about page and info to be saved

---
+++ forTieba/eachPageExample.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 1. url, name, last modify date
+# 2. floorNo, flooruser, floor date, floorid, floorcomment_num
+#    floor info
+#         time, user, info
+
+<div class="d_post_content_main">    
+<div class="p_content p_content_nameplate">
+<div class="save_face_bg_hidden save_face_bg_0">
+    <a class="save_face_card"></a>            
+</div>        
+<cc><div id="post_content_81586085655" class="d_post_content j_d_post_content  clearfix">
+            西藏要稳定，<br>邻国也要安定，<br>就是务实的目标，</div><br></cc>
+
+
+
+<div class="l_post j_l_post l_post_bright  "  data-field=
+<div class="l_post j_l_post l_post_bright noborder "  data-field='{&quot;author&quot;:{&quot;user_id&quot;:176560909,&quot;user_name&quot;:&quot;\u90a3\u6708\u90a3\u5e74&quot;,&quot;name_u&quot;:&quot;%E9%82%A3%E6%9C%88%E9%82%A3%E5%B9%B4&amp;ie=utf-8&quot;,&quot;user_sex&quot;:2,&quot;portrait&quot;:&quot;0d1be982a3e69c88e982a3e5b9b4860a&quot;,&quot;is_like&quot;:1,&quot;level_id&quot;:6,&quot;level_name&quot;:&quot;\u603b\u5175&quot;,&quot;cur_score&quot;:115,&quot;bawu&quot;:0,&quot;props&quot;:null},&quot;content&quot;:{&quot;post_id&quot;:78709357436,&quot;is_anonym&quot;:false,&quot;open_id&quot;:&quot;tieba&quot;,&quot;open_type&quot;:&quot;&quot;,&quot;date&quot;:&quot;2015-11-08 08:14&quot;,&quot;vote_crypt&quot;:&quot;&quot;,&quot;post_no&quot;:1,&quot;type&quot;:&quot;0&quot;,&quot;comment_num&quot;:0,&quot;ptype&quot;:&quot;0&quot;,&quot;is_saveface&quot;:false,&quot;props&quot;:null,&quot;post_index&quot;:0,&quot;pb_tpoint&quot;:null}}' >  
+
+
+########################################################################################
+# if get username, then get all about the user
+http://tieba.baidu.com/home/main?un=%E6%A2%A6%E5%9B%9E%E8%BE%BE%E6%97%BA&fr=frs&ie=utf-8
+http://tieba.baidu.com/home/main?un=xiaowm&fr=frs&ie=utf-8
+http://tieba.baidu.com/home/main/?un=qsk015&ie=utf-8&fr=frs
+http://tieba.baidu.com/home/main?un=%E5%BE%AE%E7%AC%91%E7%9D%80%E8%AF%B4abc&fr=frs&ie=utf-8
+########################################################################################
