73f3953ae77e9188e2778c2a260f4079e910c89a
tuouo <ltytuotuo@gmail.com>
2016-01-15 16:29:35 +0800

    remove quantitative value

--- forTieba/getItemData.py
+++ forTieba/getItemData.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from datetime import datetime, date, time
+from datetime import datetime
 import os
 
-path = os.path.join(os.getcwd(), "resource", "zangnan")
-url = "http://tieba.baidu.com/f?kw=%E4%BC%AA%E9%98%BF%E9%B2%81%E7%BA%B3%E6%81%B0%E5%B0%94%E9%82%A6&ie=utf-8&tp=0&pn="
-
 def getDateTime(item, isItem = False):
-    # print(item)
     if item.find(":") < 0 and item.find("-") < 0:        
         return None
     data = item.split("'")[5] if isItem else item
@@ -21,11 +17,11 @@ def getDateTime(item, isItem = False):
         return datetime.strptime(data, "%Y-%m-%d %H:%M:%S")
 
 def getLastData(fileName):
-    with open(os.path.join(path, fileName), "rb") as one:
+    with open(fileName, "rb") as one:
         if sum(1 for x in one) == 1:
             one.seek(0)
             return one.readline()
-    with open(os.path.join(path, fileName), "rb") as old:
+    with open(fileName, "rb") as old:
         i = -1
         while True:
             i = i - 1
@@ -35,9 +31,7 @@ def getLastData(fileName):
                 if len(datas) > 5:
                     return datas[5].decode('utf-8')
 
-def reverse(old, new):
-    oldfile = os.path.join(path, old)
-    newfile = os.path.join(path, new)
+def reverse(oldfile, newfile):
     data = []
     with open(oldfile, "r+b") as old:
         for i in old:
