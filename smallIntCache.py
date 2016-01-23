1eb455b126ca4fe86dae6d2e542db840faf20e49
tuouo <ltytuotuo@gmail.com>
2016-01-23 22:36:18 +0800

    can not change value to wanted

--- smallIntCache.py
+++ smallIntCache.py
@@ -8,18 +8,36 @@ def printIDofSameValue(a, b):
     else:
         print("%s: id%s" % (a, id(a)))
 
-a, b = 1, 1
-printIDofSameValue(a,b)       
-a, b = -6, -6
-printIDofSameValue(a,b)
-k = -7 
-a, b = k, k
-printIDofSameValue(a,b)
+def testIDs():
+    a, b = 1, 1
+    printIDofSameValue(a,b)       
+    a, b = -6, -6
+    printIDofSameValue(a,b)
+    k = -7 
+    a, b = k, k
+    printIDofSameValue(a,b)
+    
+    a = list(range(-6, 258))
+    b = list(range(-6, 258))
+    printIDofSameValue(a[0],b[0])
+    
+    for i in range(len(a)):        # [-5, 256]
+        if id(a[i]) != id(b[i]):
+            print("%s: id%s, %s: id%s" % (a[i], id(a[i]), b[i], id(b[i])))
 
-a = list(range(-6, 258))
-b = list(range(-6, 258))
-printIDofSameValue(a[0],b[0])
+def changeAtoBWhenSmall(a, b):
+    import ctypes
+    ivalOffset = ctypes.sizeof(ctypes.c_size_t) + ctypes.sizeof(ctypes.c_voidp)
+    ival = ctypes.c_int.from_address(id(a) + ivalOffset)
+    ival.value = b
 
-for i in range(len(a)):        # [-5, 256]
-    if id(a[i]) != id(b[i]):
-        print("%s: id%s, %s: id%s" % (a[i], id(a[i]), b[i], id(b[i])))
+if __name__ == '__main__':
+    # testIDs()
+    # import ctypes
+    # value = 2
+    # ivalOffset = ctypes.sizeof(ctypes.c_size_t) + ctypes.sizeof(ctypes.c_voidp)
+    # ival = ctypes.c_int.from_address(id(value) + ivalOffset)
+    # ival.value = 3
+    x = 1 + 1
+    changeAtoBWhenSmall(2, 4)
+    print(x)
