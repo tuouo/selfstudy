526c5a5fc2d98abdf6b17361d5731347177f3699
tuouo <ltytuotuo@gmail.com>
2016-01-24 21:47:49 +0800

    test scoket

--- forTieba/mainGetAllPost.py
+++ forTieba/mainGetAllPost.py
@@ -3,7 +3,7 @@
 from getPostUrl import getALLPostUrl, getNewPostUrl
 from getEachNewUrl import getOnePost
 from combinePostUrl import combineUrls
-import os
+import os, re, socket
 
 lastPage = 130
 path = os.path.join("C:/", "resource", "TiebaCrawler", "zangnan")
@@ -11,17 +11,23 @@ url = "http://tieba.baidu.com/f?kw=%E4%BC%AA%E9%98%BF%E9%B2%81%E7%BA%B3%E6%81%B0
 allUrlFile = os.path.join(path, "postUrlItem.rtf")
 newUrlFile = os.path.join(path, "newUrlItem.rtf")
 
-def fromUrlItemGetPost(path, itemFile):
+def fromUrlItemGetPost(path, itemFile, firstTime):
     with open(itemFile, "rb+") as f:
         for line in f:
             url = line.split(b"'")[1].decode('utf-8')
-        fileName = re.findall("/(\d+)", url)[0]
-        filepath = os.path.join(path, fileName)
-        if not os.path.exists(filepath):
-            os.mkdir(filepath)
-        print("****    %s    ****" % url)
-        getOnePost(url, filepath)
-        print("OK, %s" % url)
+            fileName = re.findall("/(\d+)", url)[0]
+            filepath = os.path.join(path, fileName)
+            if not os.path.exists(filepath):
+                os.mkdir(filepath)
+            url = "http://tieba.baidu.com" + url
+            print("****    %s    ****" % url)
+            # firstTime = False if os.path.exists(os.path.join(path, "text.rtf")) else True            
+            try:
+                getOnePost(url, filepath, firstTime)
+            except socket.timeout:
+
+                getOnePost(url, filepath, firstTime)
+            print("OK, %s" % url)
     
 
 if __name__ == "__main__":
@@ -29,8 +35,10 @@ if __name__ == "__main__":
         os.makedirs(path)
     if not os.path.exists(allUrlFile):
         getALLPostUrl(allUrlFile, url, lastPage -1)
-        fromUrlItemGetPost(path, allUrlFile)
+        fromUrlItemGetPost(path, allUrlFile, True)
+    
+    fromUrlItemGetPost(path, allUrlFile, True)
 
-    getNewPostUrl(allUrlFile, newUrlFile, url)
-    fromUrlItemGetPost(path, newUrlFile)
-    combineUrls(allUrlFile, newUrlFile)
+    # getNewPostUrl(allUrlFile, newUrlFile, url)
+    # fromUrlItemGetPost(path, newUrlFile)
+    # combineUrls(allUrlFile, newUrlFile)
