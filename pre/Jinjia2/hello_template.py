dfd4e336fed8917ef6c469e6823150719873045f
tuouo <ltytuotuo@gmail.com>
2016-02-23 14:30:57 +0800

    add errorhandler, by extends

--- pre/Jinjia2/hello_template.py
+++ pre/Jinjia2/hello_template.py
@@ -17,6 +17,13 @@ def index():
 def user(name):
     return render_template('user.html', name=name.title())
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('index.html')
 
 if __name__ == '__main__':
     manager.run()
