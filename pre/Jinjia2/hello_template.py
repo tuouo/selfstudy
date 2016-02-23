79e93356b29873d6e58333f505853d78fd03aa9e
tuouo <ltytuotuo@gmail.com>
2016-02-23 17:20:12 +0800

    import moment

--- pre/Jinjia2/hello_template.py
+++ pre/Jinjia2/hello_template.py
@@ -3,15 +3,18 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -23,7 +26,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('index.html')
+    return render_template('500.html')
 
 if __name__ == '__main__':
     manager.run()
