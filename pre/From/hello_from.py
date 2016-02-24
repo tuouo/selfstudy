91dfd09ba8fa5826eaee00ac4d36158642f85c44
tuouo <ltytuotuo@gmail.com>
2016-02-24 22:40:06 +0800

    hello from

---
+++ pre/From/hello_from.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import From
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name.title())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html')
+
+if __name__ == '__main__':
+    manager.run()
