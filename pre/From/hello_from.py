c13957bf493deb313e4d64dfa6f2eca1be5abfe2
tuouo <ltytuotuo@gmail.com>
2016-03-01 00:01:03 +0800

    import flash

--- pre/From/hello_from.py
+++ pre/From/hello_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -31,6 +31,9 @@ def index():
     # return render_template('index.html', form=form, name=name)
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like someone reach here before.')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
