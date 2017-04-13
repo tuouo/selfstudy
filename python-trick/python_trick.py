9367dcdc6bc3d60ec5349c4e710372a79b709613
yang luo <ltytuotuo@gmail.com>
2017-04-13 12:13:14 +0800

    show result of method in EssentialTipsAndTricks

--- python-trick/python_trick.py
+++ python-trick/python_trick.py
@@ -11,33 +11,37 @@ class EssentialTipsAndTricks:
     @staticmethod
     def print_file_path_of_imported_variables():
         import sys
-        print(sys)
+        import os
+        print("Path of sys:", sys)
+        print("Path of os:", os)
 
     @staticmethod
     def dictionary_set_comprehensions():
         test_dict = {i: i * i for i in range(9)}
         test_set = {i for i in range(9)}
-        print(test_dict, test_set)
+        print("dict & set", test_dict, test_set)
 
     @staticmethod
     def debugging_scripts():
-        import pdb
-        pdb.set_trace()
+        # import pdb
+        # pdb.set_trace()
+        pass
 
     @staticmethod
     def detect_python_version_at_runtime():
         import sys
         # if not hasattr(sys, "version_info") or sys.version_info >= (3, 5):
         if not hasattr(sys, "hexversion") or sys.hexversion != 50660080:
-            print("Oh, you are not running on Python 3.5\n")
-            sys.exit(1)
+            print("Oh, you are not running on Python 3.5")
+            # sys.exit(1)
+        print("Current version is:", sys.version_info)
 
     @staticmethod
     def use_enums():
         class Shapes:
             Circle, Square, Triangle, Quadrangle = range(4)
 
-        print(Shapes.Circle, Shapes.Triangle)
+        print("enums items:", Shapes.Circle, Shapes.Triangle)
 
     @staticmethod
     def dictionary_switch():
@@ -45,26 +49,46 @@ class EssentialTipsAndTricks:
             "sum": lambda x, y: x + y,
             "sub": lambda x, y: x - y
         }
-        print(std_calc["sum"](std_calc["sub"](8, 3), 4))
+        print("Result of dictionary switch:", std_calc["sum"](std_calc["sub"](8, 3), 4))
 
     @staticmethod
     def about_method():
         test = [1, 2, 3, 4, 2, 2, 3, 1, 4, 4, 4]
-        print(max(set(test), key=test.count))
+        print("With in max:", max(set(test), key=test.count))
 
     @staticmethod
     def reset_recursion_limit():
         import sys
-        print(sys.getrecursionlimit())
+        print("recursionlimit:", sys.getrecursionlimit())
         sys.setrecursionlimit(sys.getrecursionlimit() + 1)
-        print(sys.getrecursionlimit())
+        print("new recursionlimit:", sys.getrecursionlimit())
 
     @staticmethod
     def memory_object(x=1):
         import sys
         # python 2.7 -> 24
         # python 3.5 -> 28
-        print(sys.getsizeof(x))
+        print("memory:", sys.getsizeof(x))
+
+    @staticmethod
+    def about_chain():
+        import itertools
+        test = [[2, 3], [4, 5], [9, 0]]
+        print("chain list", list(itertools.chain.from_iterable(test)))
+
+    @staticmethod
+    def switch_from_dict():
+        def x_switch(name):
+            return x_switch._inside.get(name, None)
+
+        x_switch._inside = {"a": 1, "b": 2, "c": 3}
+        print("switch:", x_switch("a"), x_switch("c"))
+
 
 if __name__ == '__main__':
-    pass
+    # print(vars(EssentialTipsAndTricks).items())
+    for method in dir(EssentialTipsAndTricks):
+        if not method.startswith("_"):
+            fn = getattr(EssentialTipsAndTricks, method, None)
+            if callable(fn):
+                fn()
